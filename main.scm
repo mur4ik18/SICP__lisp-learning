4060b17bf20cb78acce3a6ce1fb17103f23f330c
mur4ik18 <o.o.kotov1999@gmail.com>
2023-01-26 19:16:14 +0100

    :rocket:

--- main.scm
+++ main.scm
@@ -1,8 +1,14 @@
 #lang sicp
 (define size 2)
-(define pi 3.14)
-(define circul (* 2 pi size))
-circul
+#(define pi 3.14)
+#(define circul (* 2 pi size))
+#(define (square x)(* x x))
+#(define (sum_of_square x y)(+(square x) (square y)))
 
-(define (square x)(* x x))
-(define (sum_of_square x y)(+(square x) (square y)))
+#(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+#(/
+ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+ (* (* (- 6 2)(- 2 7)) 3))
